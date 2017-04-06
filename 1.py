b0246456f4116617f373bbae1142442091d3062b
parisding <parisding@live.com>
2017-04-06 23:08:44 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,6 @@
+print('n =', 123)
+print('f =', 456.7890)
+print('s1 =', '\'Hello, world\'')
+print('s2 =', '\'Hello, \\\'Adam\\\'')
+print('s3 =', 'r\'Hello, \"Rart\"\'')
+print('s4 =', 'r\'\'\'Hello,\nLisal\'\'\'')
